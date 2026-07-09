46dde565e54b0fadd092461c2a31b58464ba80af
SANJEET YADAV <2k23.csai2312979@gmail.com>
2026-07-09 14:38:49 +0530

    Time: 0 ms (100%), Space: 52.5 MB (26.93%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int far=0;
+        for(int i=0;i<n;i++){
+            if(i>far)return false;
+            far = max(far,nums[i]+i);
+            if(far>=n-1)return true;
+        }
+        return true;
+        
+    }
+};
