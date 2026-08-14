433eebb1113cabfbeb6a007008d538e2a2fd0ca8
SANJEET YADAV <2k23.csai2312979@gmail.com>
2026-08-14 10:13:27 +0530

    Time: 0 ms (100%), Space: 43.2 MB (51.07%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prf = strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(prf)!=0){
+                prf = prf.substring(0,prf.length()-1);
+            }
+             if(prf.isEmpty()){
+            return "";
+          }
+        }
+        return prf;
+       
+
+        
+    }
+}
